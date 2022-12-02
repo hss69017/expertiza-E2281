716b2b8dfa1cd052cd749a47b7fac9fee6d149bc
naydt01 <58788092+naydt01@users.noreply.github.com>
2022-12-02 12:18:22 -0500

    Temporary Addition of all methods previous team used, in order to refactor

---
+++ app/controllers/waiting_team_controller.rb
@@ -0,0 +1,76 @@
+class WaitingTeamController < ApplicationController
+    include AuthorizationHelper
+
+    # Returns team added to waitlist the earliest
+    def first_team_in_waitlist_for_topic(topic_id)
+        waitlisted_team_for_topic = WaitlistTeam.where(topic_id: topic_id).order("created_at ASC").first
+        waitlisted_team_for_topic
+    end
+
+    # Returns FALSE if team belongs to a waitlist
+    def team_has_waitlists(team_id)
+        WaitlistTeam.where(team_id: team_id).empty?
+    end
+
+    # Returns FALSE if topic contains a waitlist
+    def topic_has_waitlists(topic_id)
+        WaitlistTeam.where(topic_id: topic_id).empty?
+    end
+
+    # Deletes all waitlist entries for a team
+    def delete_all_waitlists_for_team(team_id)
+        waitlisted_topics_for_team = get_all_waitlists_for_team team_id
+        if !waitlisted_topics_for_team.nil?
+          waitlisted_topics_for_team.destroy_all
+        else
+          ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, "Cannot find Team #{team_id} in waitlist.")
+          return false
+        end
+        return true
+    end
+
+    # Deletes all entries from a waitlist
+    def delete_all_waitlists_for_topic(topic_id)
+        waitlisted_teams_for_topic = get_all_waitlists_for_topic topic_id
+        if !waitlisted_teams_for_topic.nil?
+          waitlisted_teams_for_topic.destroy_all
+        else
+          ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, "Cannot find Topic #{topic_id} in waitlist.")
+        end
+        return true
+    end
+
+    # Returns a list of waitlisted topics for a team
+    def get_all_waitlists_for_team(team_id)
+        WaitlistTeam.joins(:topic).select('waitlist_teams.id, topic_name, team_id, topic_id, created_at').where(team_id: team_id)
+    end
+
+    # Returns a list of teams waitlisted for a topic
+    def get_all_waitlists_for_topic(topic_id)
+        WaitlistTeam.where(topic_id: topic_id)
+    end
+
+    # Adds top of queue to the topic when made availible
+    def signup_first_waitlist_team(topic_id)
+        sign_up_waitlist_team = nil
+        ApplicationRecord.transaction do
+          first_waitlist_team = first_team_in_waitlist_for_topic(topic_id)
+          unless first_waitlist_team.blank?
+            sign_up_waitlist_team = SignedUpTeam.new
+            sign_up_waitlist_team.topic_id = first_waitlist_team.topic_id
+            sign_up_waitlist_team.team_id = first_waitlist_team.team_id
+            if sign_up_waitlist_team.valid?
+              sign_up_waitlist_team.save
+              first_waitlist_team.destroy
+              delete_all_waitlists_for_team sign_up_waitlist_team.team_id
+            else
+              ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', session[:user].id, "Cannot find Topic #{topic_id} in waitlist.")
+              raise ActiveRecord::Rollback
+            end
+          end
+        end
+        sign_up_waitlist_team
+    end
+
+
+end
--- app/models/waiting_team.rb
+++ app/models/waiting_team.rb
@@ -5,6 +5,31 @@ class WaitingTeam < ApplicationRecord
   validates :topic_id, :team_id, presence: true
   validates :topic_id, uniqueness: { scope: :team_id }
 
-  
+  # The method is used to add team to waitlist table
+  def self.add_team_to_topic_waitlist(team_id, topic_id)
+    new_waitlist = WaitlistTeam.new
+    new_waitlist.topic_id = topic_id
+    new_waitlist.team_id = team_id
+    if new_waitlist.valid?
+      WaitlistTeam.create(topic_id: topic_id, team_id: team_id)
+    else
+      ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, "Team #{team_id} cannot be added to waitlist for the topic #{topic_id}")
+      ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, new_waitlist.errors.full_messages.join(" "))
+      return false
+    end
+    return true
+  end
+
+  # This method removes team in the waitlist_teams table
+  def self.remove_team_from_topic_waitlist(team_id, topic_id)
+    waitlisted_team_for_topic = WaitlistTeam.find_by(topic_id: topic_id, team_id: team_id)
+    unless waitlisted_team_for_topic.nil?
+      waitlisted_team_for_topic.destroy
+    else
+      ExpertizaLogger.info LoggerMessage.new('WaitlistTeam', user_id, "Cannot find Team #{team_id} in waitlist for the topic #{topic_id} to be deleted.")
+    end
+    return true
+  end
+
 
 end
